ab9a7c9aad13a94153e1e83a3e86837e5bc34bf3
Lamont <sl_lamont@yahoo.com>
2023-09-26 18:24:43 -0400

    Add completed lab files

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
 
+const DivPanel = () => {
+    // Select state from store whose value you want to access
+    let counterVal = useSelector(state => state.counter)
+    return (
+        <div>
+            The present value of counter is {counterVal}
+        </div>
+    );
+}
+
+export default DivPanel;
